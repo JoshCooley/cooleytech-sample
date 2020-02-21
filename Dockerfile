c23d7de888cce3e66b0353f0a3e4e9eaa15459cf
Josh Cooley <josh.cooley@smiledirectclub.com>
2020-02-21 00:05:53 -0600

    Moved the sample project to its own bespoke repo
    * New repo is called cooley.tech-sample
      - All references have been switched to the new repo name
      - Terraform directory has been removed
    * Many Docker build script changes
      - New PROJECT_NAME var, defaulted to the base name of the repo directory
      - Added 2 Dockerfile linters (dockerfilelint, hadolint)
      - There's emojis now 🧨🍺💥🤷‍♂️🐐💯💯💯

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM python:3.8
-ADD . cooley.tech
-WORKDIR cooley.tech
+
+ARG PROJECT_NAME=cooley.tech-sample
+ENV PROJECT_NAME ${PROJECT_NAME}
+
+COPY . ${PROJECT_NAME}
+WORKDIR /${PROJECT_NAME}
 
 RUN ["pip","install","-r","requirements.txt"]
 
--- build_docker_container.sh
+++ build_docker_container.sh
@@ -1,3 +1,34 @@
 #!/usr/bin/env bash
 
-docker build . -t cooley.tech:build
+PROJECT_NAME=$(basename "$(git rev-parse --show-toplevel)")
+
+linters=(dockerfilelint hadolint)
+lint_dockerfile(){
+  case $1 in
+    dockerfilelint )  # https://github.com/replicatedhq/dockerfilelint
+      docker run -v "$(pwd)"/Dockerfile:/Dockerfile \
+        replicated/dockerfilelint /Dockerfile
+      ;;
+    hadolint )  # https://github.com/hadolint/hadolint
+      docker run --rm -i hadolint/hadolint < Dockerfile
+      ;;
+    * )
+      printf '\nUsage: link_dockerfile LINTER\nAvailable LINTERs: %s' "${linters[*]}"
+      return 1
+      ;;
+  esac
+}
+
+for linter in "${linters[@]}"; do
+  echo -n "Linting Dockerfile with $linter ... "
+  if lint=$(lint_dockerfile "$linter" 2>&1); then
+    echo 'PASSED ✅'
+    docker build . \
+      --build-arg PROJECT_NAME="$PROJECT_NAME" \
+      --tag "$PROJECT_NAME":build
+  else
+    echo 'FAILED ❌'
+    echo "$lint"
+    exit 1
+  fi
+done
