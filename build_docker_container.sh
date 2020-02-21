361049ed02cf3b9d7c61c09553b0e8ebea750e67
Josh Cooley <josh.cooley@smiledirectclub.com>
2020-02-21 02:23:47 -0600

    Add python linting and testing script

--- build_docker_container.sh
+++ build_docker_container.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=$(basename "$(git rev-parse --show-toplevel)")
+PROJECT_TYPE=${PROJECT_TYPE:-Dockerfile}
 
 linters=(dockerfilelint hadolint)
-lint_dockerfile(){
+lint_project(){
   case $1 in
     dockerfilelint )  # https://github.com/replicatedhq/dockerfilelint
       docker run -v "$(pwd)"/Dockerfile:/Dockerfile \
@@ -13,22 +14,24 @@ lint_dockerfile(){
       docker run --rm -i hadolint/hadolint < Dockerfile
       ;;
     * )
-      printf '\nUsage: link_dockerfile LINTER\nAvailable LINTERs: %s' "${linters[*]}"
+      printf '\nUsage: %s LINTER\nAvailable LINTERs: %s' \
+        "${FUNCNAME[0]}" "${linters[*]}"
       return 1
       ;;
   esac
 }
 
 for linter in "${linters[@]}"; do
-  echo -n "Linting Dockerfile with $linter ... "
-  if lint=$(lint_dockerfile "$linter" 2>&1); then
+  printf '%-50s' "Linting $PROJECT_TYPE with $linter ... "
+  if lint=$(lint_project "$linter" 2>&1); then
     echo 'PASSED ✅'
-    docker build . \
-      --build-arg PROJECT_NAME="$PROJECT_NAME" \
-      --tag "$PROJECT_NAME":build
   else
     echo 'FAILED ❌'
     echo "$lint"
     exit 1
   fi
 done
+
+docker build . \
+  --build-arg PROJECT_NAME="$PROJECT_NAME" \
+  --tag "$PROJECT_NAME":build
