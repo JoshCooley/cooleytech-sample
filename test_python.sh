87d2c5a38ffbdf7f6a576e8b634cc3546e2ce007
Josh Cooley <josh.cooley@smiledirectclub.com>
2020-02-23 01:10:40 -0600

    Now we're cooking with python 🐍!
    * Fix PORT bug. `heroku local` works now!
    * Switch heroku python version to the same as my local
    * Make this work as a command-line module, e.g. `python -m cooleytech_sample`
    * Add code testing to python test script
    * TODO (Possibly?): Merge testers and linters. They're basically the same.

--- test_python.sh
+++ test_python.sh
@@ -20,6 +20,29 @@ lint_project(){
   esac
 }
 
+testers=(web)
+test_project(){
+  case $1 in
+    web)
+      echo 1
+      PORT=1234 python -um . & pid=$!
+      echo 2
+      curl --silent --show-error http://localhost:1234/
+      exit_code=$?
+      echo 4
+      kill "$pid"
+      echo 5
+      return "$exit_code"
+      echo 6
+      ;;
+    * )
+      printf '\nUsage: %s TESTER\nAvailable TESTER: %s' \
+        "${FUNCNAME[0]}" "${testers[*]}"
+      return 1
+      ;;
+  esac
+}
+
 if [[ $WITH_VENV == true ]]; then
   venv=$(mktemp -d)
   echo "Creating build venv $venv"
@@ -27,6 +50,7 @@ if [[ $WITH_VENV == true ]]; then
   echo 'Activating venv'
   . "$venv/bin/activate"
 fi
+
 pip install -r requirements.txt -r testing-requirements.txt
 for linter in "${linters[@]}"; do
   printf '%-50s' "Linting $PROJECT_TYPE with $linter ... "
@@ -38,6 +62,17 @@ for linter in "${linters[@]}"; do
     exit 1
   fi
 done
+for tester in "${testers[@]}"; do
+  printf '%-50s' "Testing $PROJECT_TYPE with $tester ... "
+  if test=$(test_project "$tester" 2>&1); then
+    echo 'PASSED ✅'
+  else
+    echo 'FAILED ❌'
+    echo "$test"
+    exit 1
+  fi
+done
+
 if [[ $WITH_VENV == true ]]; then
   echo "Deactivating venv $venv"
   deactivate
